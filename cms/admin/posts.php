cd4f81b4b5eaee0771f3cfffcf7278eb756e9b6a
Tuyen Pham <tuh39709@temple.edu>
2021-07-01 16:06:11 -0400

    add cms project

---
+++ cms/admin/posts.php
@@ -0,0 +1,29 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_nav.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to POsts
+                        </h1>
+
+                        table>thead>tr>th*2
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>
+    
