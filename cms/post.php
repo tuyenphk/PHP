90a75a86af37e36694377c546fe209e05623b889
Tuyen Pham <tuh39709@temple.edu>
2021-07-08 12:00:25 -0400

    add validation to comments

--- cms/post.php
+++ cms/post.php
@@ -64,17 +64,21 @@
                         $comment_author = $_POST['comment_author'];
                         $comment_email = $_POST['comment_email'];
                         $comment_content = $_POST['comment_content'];
-                       
-                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
-                        $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()) ";
 
-                        $add_comment_query = mysqli_query($connection, $query);
-                        confirmQuery($add_comment_query);
+                        if(!empty($comment_author) && !empty($comment_email) && !empty($comment_content)){
+                            $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                            $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()) ";
+    
+                            $add_comment_query = mysqli_query($connection, $query);
+                            confirmQuery($add_comment_query);
 
-                        // Increasing comments count 
-                        $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                        $query .= "WHERE post_id = $the_post_id ";
-                        $update_comment_count = mysqli_query($connection, $query);
+                            // Increasing comments count 
+                            $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                            $query .= "WHERE post_id = $the_post_id ";
+                            $update_comment_count = mysqli_query($connection, $query);
+                        } else {
+                            echo "<script>alert('Fields can not be empty')</script>";
+                        }
                     }
                 ?>
 
