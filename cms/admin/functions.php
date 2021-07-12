f4502ebfaccb5c1ad57bfc33bee44b6984665f9e
Tuyen Pham <tuh39709@temple.edu>
2021-07-12 16:44:36 -0400

    sending email

--- cms/admin/functions.php
+++ cms/admin/functions.php
@@ -84,4 +84,10 @@
 
     users_online();
 
+    function escape($string){
+        global $connection;
+        return mysqli_real_escape_string($connection, trim(strip_tags($string)));
+
+    }
+
 ?>
