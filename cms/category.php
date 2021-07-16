c70327eaa8ec833459fe9988e7c3b0dbc91bd82d
Tuyen Pham <tuh39709@temple.edu>
2021-07-16 09:45:02 -0400

    nothing update

--- cms/category.php
+++ cms/category.php
@@ -16,47 +16,54 @@
                     if (isset($_GET['category'])){
                         $post_category_id = $_GET['category'];
 
-                    $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published' ";
-                    $select_all_posts_query = mysqli_query($connection, $query);
+                        // if (isAdmin($_SESSION['username'])){
+                        //     $stm1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ? ");
+                        // } else {
+                        //     $stm2 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ? ");
+                        //     $published = 'published';
+                        // }
 
-                    if (mysqli_num_rows($select_all_posts_query) < 1){
-                        echo "<h1 class='text-center'>No Posts available</h1>";
-                    } else {
+                        $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published' ";
+                        $select_all_posts_query = mysqli_query($connection, $query);
 
-                        while ($row = mysqli_fetch_assoc($select_all_posts_query)){
-                            $post_id = $row['post_id'];
-                            $post_title = $row['post_title'];
-                            $post_author = $row['post_author'];
-                            $post_date = $row['post_date'];
-                            $post_image = $row['post_image'];
-                            $post_content = substr($row['post_content'],0,100);
+                        if (mysqli_num_rows($select_all_posts_query) < 1){
+                            echo "<h1 class='text-center'>No Posts available</h1>";
+                        } else {
 
-                            echo "<li><a href='#'>{$post_title}</a></li>"; 
-                            ?>
+                            while ($row = mysqli_fetch_assoc($select_all_posts_query)){
+                                $post_id = $row['post_id'];
+                                $post_title = $row['post_title'];
+                                $post_author = $row['post_author'];
+                                $post_date = $row['post_date'];
+                                $post_image = $row['post_image'];
+                                $post_content = substr($row['post_content'],0,100);
 
-                        <h1 class="page-header">
-                            Page Heading
-                            <small>Secondary Text</small>
-                        </h1>
+                                echo "<li><a href='#'>{$post_title}</a></li>"; 
+                                ?>
 
-                        <!-- First Blog Post -->
-                        <h2>
-                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
-                        </h2>
-                        <p class="lead">
-                            by <a href="index.php"><?php echo $post_author; ?></a>
-                        </p>
-                        <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
-                        <hr>
-                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                        <hr>
-                        <p><?php echo $post_content; ?></p>
-                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                            <h1 class="page-header">
+                                Page Heading
+                                <small>Secondary Text</small>
+                            </h1>
 
-                        <hr>
+                            <!-- First Blog Post -->
+                            <h2>
+                                <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                            </h2>
+                            <p class="lead">
+                                by <a href="index.php"><?php echo $post_author; ?></a>
+                            </p>
+                            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                            <hr>
+                            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                            <hr>
+                            <p><?php echo $post_content; ?></p>
+                            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                    <?php }}} else {
-                        header("Locations: index.php");
+                            <hr>
+
+                        <?php }}} else {
+                            header("Locations: index.php");
                     }
                     ?>
                 
