f0297208be78cbff6d3f9e6ceda88c8e0a6a2502
Tuyen Pham <tuh39709@temple.edu>
2021-07-16 14:15:39 -0400

    instal third party library with composer

--- cms/forgot.php
+++ cms/forgot.php
@@ -18,6 +18,9 @@
                     mysqli_stmt_bind_param($stmt, "s", $email);
                     mysqli_stmt_execute($stmt);
                     mysqli_stmt_close($stmt);
+
+                    
+
                 } else {
                     echo "Wrong";
                 }
---
+++ cms/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd255d65d93a69356dc020669510ce5fc6d31196e',
+        'name' => 'pham_t/cms',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pham_t/cms' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd255d65d93a69356dc020669510ce5fc6d31196e',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.0',
+            'version' => '6.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
