b0c4c93d414b16c75db613e76301e7e6f34cdc8e
Tuyen Pham <tuh39709@temple.edu>
2021-07-16 16:57:10 -0400

    update new reset page and pulling data

--- cms/reset.php
+++ cms/reset.php
@@ -2,11 +2,24 @@
 <?php  include "includes/header.php"; ?>
 
 <?php
-    if (!isset($_GET['email']) && !isset($_GET['token'])){
-        redirect('index');
+    // if (!isset($_GET['email']) && !isset($_GET['token'])){
+    //     redirect('index');
+    // }
+
+    $token = "662fe8746f37a1e454568fe3006306234379957dec8d6b24dc226f724d9ca9dad89c76e768328bd538884211b0976a56f832";
+    if ($stmt = mysqli_prepare($connection, 'SELECT user_name, user_email, token FROM users WHERE token=? ')){
+        mysqli_stmt_bind_param($stmt, "s", $token);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $user_name, $user_email, $token);
+        mysqli_stmt_fetch($stmt);
+        mysqli_stmt_close($stmt);
+
+        // echo $user_name;
+
+        // if ($_GET['token'] !== $token || $_GET['email'] !== $email){
+        //     redirect('index');
+        // }
     }
-
-
 
 ?>
 
@@ -33,20 +46,24 @@
 
                                         <div class="form-group">
                                             <div class="input-group">
-                                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                                                <input id="email" name="email" placeholder="email address" class="form-control"  type="email">
+                                                <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+                                                <input id="password" name="password" placeholder="Enter the new password" class="form-control"  type="password">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                            <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok color-blue"></i></span>
+                                                <input id="confirmPassword" name="confirmPassword" placeholder="Enter the confirmed password" class="form-control"  type="password">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
                                         </div>
 
                                         <input type="hidden" class="hide" name="token" id="token" value="">
                                     </form>
 
                                 </div><!-- Body-->
-
-                                <h2>Please check your email</h2>
 
                         </div>
                     </div>
