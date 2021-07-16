8446698677fdf980cd333c81d5580677f60720a4
Tuyen Pham <tuh39709@temple.edu>
2021-07-16 11:28:11 -0400

    forgot password page

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -33,6 +33,10 @@
                                     <button class="btn btn-primary" name="login" type="submit">Submit</button>
                                 </span>
                             </div>
+                            &nbsp;
+                            <div class="form-group">
+                                <a href="forgot.php?forgot=<?php echo uniqid(true); ?>">Forgot Password</a>
+                            </div>
                         </form> 
 
                     <?php endif; ?>
